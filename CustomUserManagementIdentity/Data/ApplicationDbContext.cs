7ba61c3485d4493f6485a48f4b397f45788b82f0
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-08-14 09:30:26 +0300

    Renaming The Default Identity Tables

--- CustomUserManagementIdentity/Data/ApplicationDbContext.cs
+++ CustomUserManagementIdentity/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,18 @@ namespace CustomUserManagementIdentity.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+                   builder.Entity<IdentityUser>().ToTable("Users", "security");
+            builder.Entity<IdentityRole>().ToTable("Roles", "security");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "security");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims", "security");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins", "security");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims", "security");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens", "security");
         }
     }
 }
