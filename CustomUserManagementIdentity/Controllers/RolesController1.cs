6a50198d7de7e1dc4e5a0ad8d4f845fa31efa625
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-08-16 14:28:48 +0300

    View Available Roles

---
+++ CustomUserManagementIdentity/Controllers/RolesController1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace CustomUserManagementIdentity.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class RolesController : Controller
+    {
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+    }
+       
+}
