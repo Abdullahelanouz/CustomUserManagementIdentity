073c58d4768d96a9101d9407ec0343b6b97fc059
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-08-18 02:17:57 +0300

    Manage User Roles (Add/Remove) Part2

--- CustomUserManagementIdentity/Controllers/UsersController.cs
+++ CustomUserManagementIdentity/Controllers/UsersController.cs
@@ -55,6 +55,28 @@ namespace CustomUserManagementIdentity.Controllers
 
             return View(viewModel);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ManageRoles(UserRolesViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.UserId);
+
+            if (user == null)
+                return NotFound();
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            foreach (var role in model.Roles)
+            {
+                if (userRoles.Any(r => r == role.RoleName) && !role.IsSelected)
+                    await _userManager.RemoveFromRoleAsync(user, role.RoleName);
+
+                if (!userRoles.Any(r => r == role.RoleName) && role.IsSelected)
+                    await _userManager.AddToRoleAsync(user, role.RoleName);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
 
     }
 }
