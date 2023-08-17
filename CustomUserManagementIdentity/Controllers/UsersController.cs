0eb183022ef9c8b6c8cadc1e4ebfc885d9b0ebb5
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-08-17 18:24:30 +0300

    List All Users With Assigned Roles

---
+++ CustomUserManagementIdentity/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using CustomUserManagementIdentity.Models;
+using CustomUserManagementIdentity.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomUserManagementIdentity.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.Select(user => new UserViewModel
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                UserName = user.UserName,
+                Email = user.Email,
+                Roles = _userManager.GetRolesAsync(user).Result
+            }).ToListAsync();
+
+            return View(users);
+        }
+    }
+}
