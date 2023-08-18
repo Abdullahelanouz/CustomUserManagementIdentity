2e4b6741710b26a8a792d37c33278ad3cd0b37ba
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-08-18 12:40:55 +0300

    Modify an existing user

---
+++ CustomUserManagementIdentity/ViewModels/profileFormViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CustomUserManagementIdentity.ViewModels
+{
+    public class profileFormViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+
+        [Display(Name = "first name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+
+        [Display(Name = "Last name")]
+        public string lastName { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+    }
+}
